ff9ec3f20d64d650966cbf77807d5f1451bb3259
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-05 09:55:41 +0100

    style(sign-up) updated sign up text and login link

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,13 +1,18 @@
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Link } from 'expo-router';
 
 const SignUp = () => {
   return (
     <View className="flex-1 bg-[#F6F6F6]">
-      <View className="flex-row justify-between">
-        <Text className="font-nunito-bold">Sign Up</Text>
-        <Pressable>
-          <Text>Log in</Text>
-        </Pressable>
+      <View className="flex-row mt-10 items-center justify-center">
+        <Text className="font-nunito-bold mr-10 h-[55px] w-[167px] text-[40px] leading-[55px]">
+          Sign Up
+        </Text>
+        <View className="mt-4">
+          <Link href="/">
+            <Text className="font-nunito-extra-bold text-[#FF5C00] h-[19px] w-[59px] text-[14px]  leading-[19px]">Log in {'>'}</Text>
+          </Link>
+        </View>
       </View>
       <Text>Name</Text>
       <TextInput></TextInput>
