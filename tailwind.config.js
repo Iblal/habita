d71e5119ca7e2bd1a42c5550fcfc693bb62bb471
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-05 09:06:06 +0100

    style(custom-fonts) updated custom font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                'nunito-bold': ['Nunito-Bold', 'sans-serif'],
+                'nunito-bold': ['nunito-bold', 'sans-serif'],
             },
         },
     },
