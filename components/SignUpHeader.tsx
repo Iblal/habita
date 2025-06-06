0125b182719a5b09843725a2cf55222373677a23
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-06 19:42:46 +0100

    feat(sign-up) added login redirection

--- components/SignUpHeader.tsx
+++ components/SignUpHeader.tsx
@@ -2,10 +2,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 const SignUpHeader = () => {
+
   return (
     <View className="mb-10 flex-row items-baseline justify-center gap-10">
       <Text className="font-nunito-bold text-[40px]">Sign Up</Text>
-      <Link href="/">
+      <Link href="/(auth)/LogIn">
         <Text className="font-nunito-extra-bold text-[14px] text-[#FF5C00]">Log in {'>'}</Text>
       </Link>
     </View>
