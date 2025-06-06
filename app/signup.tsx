a8f7ca7df0073a858465bfc7a991bc7cff3451a1
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-06 12:42:06 +0100

    style(sign-up) stylised the sign-up page

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,29 +1,41 @@
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { Link } from 'expo-router';
 
 const SignUp = () => {
   return (
-    <View className="flex-1 bg-[#F6F6F6]">
-      <View className="flex-row mt-10 items-center justify-center">
-        <Text className="font-nunito-bold mr-10 h-[55px] w-[167px] text-[40px] leading-[55px]">
+    <View className="flex-1 bg-[#f6f6f6b1]">
+      <View className="mt-10 flex-row items-center justify-center">
+        <Text className="mr-10 h-[55px] w-[167px] font-nunito-bold text-[40px] leading-[55px]">
           Sign Up
         </Text>
         <View className="mt-4">
           <Link href="/">
-            <Text className="font-nunito-extra-bold text-[#FF5C00] h-[19px] w-[59px] text-[14px]  leading-[19px]">Log in {'>'}</Text>
+            <Text className="h-[19px] w-[59px] font-nunito-extra-bold text-[14px] leading-[19px]  text-[#FF5C00]">
+              Log in {'>'}
+            </Text>
           </Link>
         </View>
       </View>
-      <Text>Name</Text>
-      <TextInput></TextInput>
-      <Text>Email</Text>
-      <TextInput></TextInput>
-      <Text>Password</Text>
-      <TextInput></TextInput>
-      <Text>Password Confirmation</Text>
-      <TextInput></TextInput>
-      <Pressable>
-        <Text>Sign Up</Text>
+      <View className="mt-10">
+        <Text className="ml-5 font-nunito-semi-bold text-[#666666]">Name</Text>
+        <TextInput className="m-5 mt-3 rounded-md border border-[#EDEDED] bg-white"></TextInput>
+      </View>
+      <Text className="ml-5 font-nunito-semi-bold text-[#666666]">Email</Text>
+      <TextInput className="m-5 mt-3 rounded-md border border-[#EDEDED] bg-white"></TextInput>
+      <Text className="ml-5 font-nunito-semi-bold text-[#666666]">Password</Text>
+      <TextInput className="m-5 mt-3 rounded-md border border-[#EDEDED] bg-white"></TextInput>
+      <Text className="ml-5 font-nunito-semi-bold text-[#666666]">Password Confirmation</Text>
+      <TextInput className="m-5 mt-3 rounded-md border border-[#EDEDED] bg-white"></TextInput>
+      <Pressable className="m-5 overflow-hidden rounded-md">
+        <LinearGradient
+          className="px-6 py-5"
+          colors={['#e6a05c', '#ff7a18', '#ff5e00']}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 1 }}
+        >
+          <Text className="text-center font-nunito-extra-bold text-white">Sign Up</Text>
+        </LinearGradient>
       </Pressable>
       <Text>Or sign up with:</Text>
       <Pressable>
