eedc35c0b5f5b9c421a8f8be0b173a76fd6317bb
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-06 19:01:39 +0100

    style(sign-up) added margin to sign-up button

--- app/signup.tsx
+++ app/signup.tsx
@@ -7,12 +7,14 @@ import SignUpHeader from '~/components/SignUpHeader';
 const SignUp = () => {
   return (
     <View className="flex-1 bg-[#f6f6f6b1] pt-10">
-      <SignUpHeader/>
+      <SignUpHeader />
       <InputField label="Name" />
       <InputField label="Email" />
       <InputField label="Password" />
       <InputField label="Password confirmation" />
-      <FormSubmitBtn label="Sign Up" onPress={() => console.log('Sign Up Pressed')} />
+      <View className="mt-5">
+        <FormSubmitBtn label="Sign Up" onPress={() => console.log('Sign Up Pressed')} />
+      </View>
       <View className="my-8 items-center">
         <Text className="text-gray-500">Or sign up with:</Text>
       </View>
