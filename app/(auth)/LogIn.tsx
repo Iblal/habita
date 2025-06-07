8e963af8eda4161f3eed2be37e8f44fc95dd9d10
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-07 11:24:22 +0100

    feat(login) added login form submission

--- app/(auth)/LogIn.tsx
+++ app/(auth)/LogIn.tsx
@@ -7,15 +7,27 @@ import { useState } from 'react';
 import RememberMeAndForgot from '~/components/RememberMeAndForget';
 
 const LogIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
+
+  function handleLogin() {
+    console.log('Sign Up Pressed');
+    console.log('The values:', email, password);
+  }
 
   return (
     <View className="flex-1 bg-[#f6f6f6b1] pt-10">
       <LoginHeader />
-      <InputField label="Email" />
-      <InputField label="Password" />
+      <InputField label="Email" value={email} onChangeText={(text) => setEmail(text)} />
+      <InputField
+        label="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+      />
       <View className="mt-5">
-        <FormSubmitBtn label="Log In" onPress={() => console.log('Log In Pressed')} />
+        <FormSubmitBtn label="Log In" onPress={handleLogin} />
       </View>
       <View className="mt-5">
         <RememberMeAndForgot rememberMe={rememberMe} setRememberMe={setRememberMe} />
