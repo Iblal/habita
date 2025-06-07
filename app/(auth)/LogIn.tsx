99ad46f5e6ab93424c68a7912b8f4e5a90ab0ac1
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-07 09:59:09 +0100

    refactor(log-in) utlized log in components

--- app/(auth)/LogIn.tsx
+++ app/(auth)/LogIn.tsx
@@ -1,30 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 import InputField from '~/components/InputField';
 import LoginHeader from '~/components/LoginHeader';
-import { Checkbox } from 'expo-checkbox';
-import { Link } from 'expo-router';
 import FormSubmitBtn from '~/components/FormSubmitBtn';
 import GoogleAuthBtn from '~/components/GoogleAuthBtn';
+import { useState } from 'react';
+import RememberMeAndForgot from '~/components/RememberMeAndForget';
 
 const LogIn = () => {
+  const [rememberMe, setRememberMe] = useState(false);
+
   return (
     <View className="flex-1 bg-[#f6f6f6b1] pt-10">
       <LoginHeader />
       <InputField label="Email" />
       <InputField label="Password" />
-      <View className="mt-4 flex-row items-center justify-between px-6">
-        <View className="flex-row">
-          <Checkbox></Checkbox>
-          <Text className="ml-2 text-base text-[#666666]">Remember me</Text>
-        </View>
-        <Link href="/">
-          <Text className="font-nunito-semi-bold text-[#FF5C00]">Forgot password?</Text>
-        </Link>
-      </View>
-      <View className='mt-12'>
+      <View className="mt-5">
         <FormSubmitBtn label="Log In" onPress={() => console.log('Log In Pressed')} />
       </View>
-            <View className="my-8 items-center">
+      <View className="mt-5">
+        <RememberMeAndForgot rememberMe={rememberMe} setRememberMe={setRememberMe} />
+      </View>
+      <View className="my-8 items-center">
         <Text className="text-gray-500">Or sign in with:</Text>
       </View>
       <GoogleAuthBtn />
