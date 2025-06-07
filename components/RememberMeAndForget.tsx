992f0ac43bfbd5d127ed0201b27e8caf8038746e
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-07 09:58:12 +0100

    feat(log-in) added login components

---
+++ components/RememberMeAndForget.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Checkbox } from 'expo-checkbox';
+import { Link } from 'expo-router';
+
+interface RememberMeAndForgotProps {
+  rememberMe: boolean;
+  setRememberMe: (value: boolean) => void; 
+}
+
+const RememberMeAndForgot = ({rememberMe, setRememberMe} : RememberMeAndForgotProps) => {
+  return (
+    <View className="mt-4 flex-row items-center justify-between px-6">
+      <View className="flex-row">
+        <Checkbox color={'#FF8C00'} value={rememberMe} onValueChange={setRememberMe}></Checkbox>
+        <Text className="ml-2 text-base text-[#666666]">Remember me</Text>
+      </View>
+      <Link href="/">
+        <Text className="font-nunito-semi-bold text-[#FF5C00]">Forgot password?</Text>
+      </Link>
+    </View>
+  );
+};
+
+export default RememberMeAndForgot;
+
+const styles = StyleSheet.create({});
---
+++ components/SignInDivider.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignInDivider = () => {
+  return (
+    <View>
+      <Text>SignInDivider</Text>
+    </View>
+  )
+}
+
+export default SignInDivider
+
+const styles = StyleSheet.create({})
