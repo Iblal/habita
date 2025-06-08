14ef6f9a70f9eee31a38b8e898a0ad412c0f7bbe
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-08 19:59:02 +0100

    feat(home) added home components

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,4 +1,7 @@
 import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { Entypo } from '@expo/vector-icons';
+import Checkbox from 'expo-checkbox';
 
 const Home = () => {
   const today = new Date();
@@ -19,11 +22,26 @@ const Home = () => {
         <Text className="font-nunito-semi-bold text-3xl">Hello, </Text>
         <Text className="font-nunito-semi-bold text-3xl text-primary-orange">Iblal!</Text>
       </View>
-      <View className="flex-row justify-evenly items-center bg-orange-400 rounded-md mr-5 mt-5 p-10">
+      <View className="mr-5 mt-5 flex-row items-center justify-evenly rounded-md bg-orange-400 p-10">
         <Text className="font-nunito-bold text-xl text-white">70%</Text>
         <View>
           <Text className="font-nunito-bold text-xl text-white">3 of 5 habits</Text>
           <Text className="font-nunito-medium text-xl text-white">completed today!</Text>
+        </View>
+      </View>
+      <View className="mr-5 mt-5 rounded bg-white p-5">
+        <View className="flex-row justify-between">
+          <Text className="font-nunito-bold text-xl">Today's habits</Text>
+          <Link href="/">
+            <Text className="font-nunito-bold text-primary-orange">See all</Text>
+          </Link>
+        </View>
+        <View className="flex-row items-center justify justify-between bg-[#EDFFF4] mt-5 p-5 rounded">
+          <Text className='text-[#37C871] font-nunito-bold'>Meditating</Text>
+          <View className='flex-row items-center'>
+            <Checkbox></Checkbox>
+            <Entypo name="dots-three-vertical" size={16} color="grey" />
+          </View>
         </View>
       </View>
     </View>
