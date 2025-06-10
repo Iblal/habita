2f84670143cad0bf36f3b630949d66ef12603a1a
iblalahmed12 <iblalahmed12@gmail.com>
2025-06-10 08:40:12 +0100

    feat(added-habit-success) added added-habit-success screen

--- app/SuccessfullyAddedHabit.tsx
+++ app/SuccessfullyAddedHabit.tsx
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native';
+import FormSubmitBtn from '~/components/FormSubmitBtn';
 
 const SuccessfullyAddedHabit = () => {
   return (
-    <View>
-      <Text>SuccessfullyAddedHabit</Text>
+    <View className="flex-1 justify-center">
+      <View className="flex-row justify-center">
+        <Text className="font-nunito-bold text-4xl">Done!</Text>
+      </View>
+      <View className="mt-5 flex-row justify-center">
+        <Text className="font-nunito-semi-bold">New habit goal has been added</Text>
+      </View>
+      <View className="flex-row justify-center">
+        <Text className="font-nunito-semi-bold">Let's do our best to achieve your goal!</Text>
+      </View>
+      <View className='mt-5'>
+        <FormSubmitBtn label="Ok" />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default SuccessfullyAddedHabit
+export default SuccessfullyAddedHabit;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
